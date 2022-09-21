84476b5f933c707b5bcea54854f9f5f7acab694d
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2022-09-22 00:30:24 +0500

    I have built a layout with the Container, Flex, and VStack Component in Chakra UI.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,33 @@
-import type { NextPage } from 'next'
+import type { NextPage } from "next";
+import { Container, Flex, VStack } from "@chakra-ui/react";
 // import Head from 'next/head'
 // import Image from 'next/image'
 // import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
-    <div>     
-      Hello nextjs
-    </div>    
-  )
-}
+    <Container maxW="container.xl" p={0}>
+      <Flex h="100vh" py={20}>
+        <VStack
+          w="full"
+          h="full"
+          p={10}
+          spacing={10}
+          alignItems="flex-start"
+        ></VStack>
 
-export default Home
+        <VStack
+          w="full"
+          h="full"
+          p={10}
+          spacing={10}
+          alignItems="flex-start"
+          bg='gray.50'
+        ></VStack>
+      </Flex>
+    </Container>
+  );
+};
+
+export default Home;
+
