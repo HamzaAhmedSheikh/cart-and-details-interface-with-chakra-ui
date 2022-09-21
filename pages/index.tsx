3357777a68634501860d2bea4bc1e741b2b8d152
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2022-09-22 02:05:41 +0500

    I have built a 2-Column Form with the SimpleGrid, FormControl, and Input Component in Chakra UI.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,15 @@
 import type { NextPage } from "next";
 import { Container, Flex, VStack } from "@chakra-ui/react";
-// import Head from 'next/head'
-// import Image from 'next/image'
+import { Details } from "../src/sections/details";
+import { Cart } from "../src/sections/cart";
 // import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
     <Container maxW="container.xl" p={0}>
       <Flex h="100vh" py={20}>
-        <VStack
-          w="full"
-          h="full"
-          p={10}
-          spacing={10}
-          alignItems="flex-start"
-        ></VStack>
-
-        <VStack
-          w="full"
-          h="full"
-          p={10}
-          spacing={10}
-          alignItems="flex-start"
-          bg='gray.50'
-        ></VStack>
+        <Details />
+        <Cart />
       </Flex>
     </Container>
   );
